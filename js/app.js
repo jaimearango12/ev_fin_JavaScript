ccb429f6a6f2addb1368fbd498976f23aaef4765
jaimearango12 <jarango_12@hotmail.com>
2018-06-05 16:08:22 -0500

    avance del la calculadora

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
-var calculadora =(function operaciones(){
+var calculadora = {
 
-  function seleccionBoton(){
-    document.getElementsByClassName('tecla').style="width 18%;";
-  }
-  document.getElementsByClassName('tecla').onclick=seleccionBoton;
+	visor: document.getElementById("display"),
+	valorVisor: "0",
+	operacion: "",
+	primerValor: 0,
+	segundoValor: 0,
+	ultimoValor: 0,
+	resultado: 0,
+	auxTeclaIgual: false, // Para permitir ingreso consecutivo
 
-})
+	init: (function(){
+		this.asignarEventosFormatoBotones(".tecla");
+		this.asignarEventosaFuncion();
+	}),
+
+	//Eventos de formato de botones
+
+	asignarEventosFormatoBotones: function(selector){
+		var x = document.querySelectorAll(selector);
+		for (var i = 0; i<x.length;i++) {
+			x[i].onmouseover = this.eventoAchicaBoton;
+			x[i].onmouseleave = this.eventoVuelveBoton;
+		};
+	},
+
+	eventoAchicaBoton: function(event){
+		calculadora.AchicaBoton(event.target);
+	},
+
+	eventoVuelveBoton: function(event){
+		calculadora.AumentaBoton(event.target);
+	},
+
+	
